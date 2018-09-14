96a0d93300d6aa0e2e80dd3e9c5491abaa610040
jamiesweeney <321.jamiesweeney.321@gmail.com>
2018-09-14 10:02:43 +0100

    Added bus stop

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BusStopComponent } from './bus-stop/bus-stop.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BusStopComponent
   ],
   imports: [
     BrowserModule
